a16b96e8bee921c6f69c21fb23db3ebdab7340b7
ayush agarwal <ayushagarwal8190@gmail.com>
2023-05-26 00:34:50 +0530

    ch

--- frontend/src/component/Welcome.js
+++ frontend/src/component/Welcome.js
@@ -11,7 +11,7 @@ const Welcome = (props) => {
       style={{ padding: "30px", minHeight: "93vh" }}
     >
       <Grid item>
-        <Typography variant="h2">Welcome to Job Portal made by AYUSH AGARWAL.</Typography>
+        <Typography variant="h2">Welcome to Job Portal made by AYUSH AGARWAL</Typography>
       </Grid>
     </Grid>
   );
